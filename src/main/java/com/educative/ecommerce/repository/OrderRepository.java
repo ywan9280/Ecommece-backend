f4f2796c67e67f07fb10c035c5ff6128d6dbac17
Yuhao <ywan9280@uni.sydney.edu.au>
2024-11-07 18:07:46 +1100

    code

---
+++ src/main/java/com/educative/ecommerce/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.educative.ecommerce.repository;
+
+import com.educative.ecommerce.model.Order;
+import com.educative.ecommerce.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository  extends JpaRepository<Order, Integer> {
+    List<Order> findAllByUserOrderByCreatedDateDesc(User user);
+
+}
